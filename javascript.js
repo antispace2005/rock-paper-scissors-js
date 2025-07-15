9aaa91911d824c016621321545cd82376e5082a3
antispace2005 <145271262+antispace2005@users.noreply.github.com>
2025-07-15 19:24:31 +0300

    Add Alert The messages along with the console

--- javascript.js
+++ javascript.js
@@ -12,7 +12,6 @@ function getHumanChoice() {
     return prompt(`1:Rock\n2:Paper\n3:Scissors`, 1)
 }
 
-
 function humanChoiceToInt(humanChoice) {
     if (humanChoice == "1" || humanChoice.toLowerCase() == "rock") {
         humanChoice = 1
@@ -24,37 +23,43 @@ function humanChoiceToInt(humanChoice) {
     return humanChoice
 }
 
-
 function playRound(humanChoice, computerChoice) {
     //1 for rock
     //2 for paper
     //3 for scissors
     if (humanChoice == computerChoice) {
         console.log("Draw")
+        alert("Draw")
     }
     else if (humanChoice == 1 && computerChoice == 2) {
         computerScore += 1
         console.log("You lose! Paper beats Rock")
+        alert("You lose! Paper beats Rock")
     }
     else if (humanChoice == 1 && computerChoice == 3) {
         humanScore += 1
         console.log("You Win! Rock beats Scissors")
+        alert("You Win! Rock beats Scissors")
     }
     else if (humanChoice == 2 && computerChoice == 1) {
         humanScore += 1
         console.log("You Win! Paper beats Rock")
+        alert("You Win! Paper beats Rock")
     }
     else if (humanChoice == 2 && computerChoice == 3) {
         computerScore += 1
         console.log("You Lose! Scissors beats Paper")
+        alert("You Lose! Scissors beats Paper")
     }
     else if (humanChoice == 3 && computerChoice == 1) {
         computerScore += 1
         console.log("You Lose! Rock beats Scissors")
+        alert("You Lose! Rock beats Scissors")
     }
     else if (humanChoice == 3 && computerChoice == 2) {
         humanScore += 1
         console.log("You Win! Scissors beats Paper")
+        alert("You Win! Scissors beats Paper")
     }
 }
 
@@ -63,16 +68,19 @@ function playGame() {
         const humanSelection = humanChoiceToInt(getHumanChoice());
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection)
-
     }
+
     console.log(`Score is
     You:    ${humanScore}
     Computer: ${computerScore}`)
 
+
     if (humanScore > computerScore) {
         console.log("You Win!!")
+        alert(`Score is\nYou:    ${humanScore}\nComputer: ${computerScore}\n\nYou Win!!`)
     } else {
         console.log("Computer Wins!!")
+        alert(`Score is\nYou:    ${humanScore}\nComputer: ${computerScore}\n\nComputer Wins!!`)
     }
 }
 
