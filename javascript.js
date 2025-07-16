bbc1630044dc0f16b518522d6224f2804df036f8
antispace2005 <145271262+antispace2005@users.noreply.github.com>
2025-07-16 23:10:53 +0300

    Add UI to the game

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@ console.log("Hello World")
 
 let humanScore = 0
 let computerScore = 0
-let winCondition = 3
-
+let winCondition = 5
+let lastRoundWinner = 0
 function getComputerChoice() {
     return parseInt(Math.random() * 3) + 1
 }
@@ -27,61 +27,103 @@ function playRound(humanChoice, computerChoice) {
     //1 for rock
     //2 for paper
     //3 for scissors
+
     if (humanChoice == computerChoice) {
-        console.log("Draw")
-        alert("Draw")
+        const ChoiceText = humanChoice == 1 ? "Rock" : humanChoice == 2 ? "Scissors" : "Paper"
+        lastRoundWinner = 0
+
+        return `Draw! ${ChoiceText} can't beat ${ChoiceText}`
     }
     else if (humanChoice == 1 && computerChoice == 2) {
         computerScore += 1
-        console.log("You lose! Paper beats Rock")
-        alert("You lose! Paper beats Rock")
+        lastRoundWinner = -1
+        return "You lose! Paper beats Rock"
     }
     else if (humanChoice == 1 && computerChoice == 3) {
         humanScore += 1
-        console.log("You Win! Rock beats Scissors")
-        alert("You Win! Rock beats Scissors")
+        lastRoundWinner = 1
+
+        return "You Win! Rock beats Scissors"
     }
     else if (humanChoice == 2 && computerChoice == 1) {
         humanScore += 1
-        console.log("You Win! Paper beats Rock")
-        alert("You Win! Paper beats Rock")
+        lastRoundWinner = 1
+
+        return "You Win! Paper beats Rock"
     }
     else if (humanChoice == 2 && computerChoice == 3) {
         computerScore += 1
-        console.log("You Lose! Scissors beats Paper")
-        alert("You Lose! Scissors beats Paper")
+        lastRoundWinner = -1
+
+        return "You Lose! Scissors beats Paper"
     }
     else if (humanChoice == 3 && computerChoice == 1) {
         computerScore += 1
-        console.log("You Lose! Rock beats Scissors")
-        alert("You Lose! Rock beats Scissors")
+        lastRoundWinner = -1
+
+        return "You Lose! Rock beats Scissors"
     }
     else if (humanChoice == 3 && computerChoice == 2) {
         humanScore += 1
-        console.log("You Win! Scissors beats Paper")
-        alert("You Win! Scissors beats Paper")
+        lastRoundWinner = 1
+
+        return "You Win! Scissors beats Paper"
     }
 }
 
-function playGame() {
-    while (humanScore < winCondition && computerScore < winCondition) {
-        const humanSelection = humanChoiceToInt(getHumanChoice());
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection)
+
+const rockBtn = document.querySelector("#rock")
+const paperBtn = document.querySelector("#paper")
+const scissorsBtn = document.querySelector("#scissors")
+const roundsLog = document.querySelector("#roundsLog")
+const humanScoreElement = document.querySelector("#humanScore")
+const computerScoreElement = document.querySelector("#computerScore")
+const winner = document.querySelector("#winner")
+
+
+
+function playButton(choice) {
+    const round = document.createElement("li")
+    round.textContent = playRound(choice, getComputerChoice())
+    roundsLog.appendChild(round)
+    if (lastRoundWinner == 1){
+        round.style.color = 'green'
+    }else if (lastRoundWinner == -1){
+                round.style.color = 'red'
+
+    }else{
+                round.style.color = 'orange'
+
     }
 
-    console.log(`Score is
-    You:    ${humanScore}
-    Computer: ${computerScore}`)
+    if (!(humanScore < winCondition && computerScore < winCondition)) {
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
 
 
-    if (humanScore > computerScore) {
-        console.log("You Win!!")
-        alert(`Score is\nYou:    ${humanScore}\nComputer: ${computerScore}\n\nYou Win!!`)
-    } else {
-        console.log("Computer Wins!!")
-        alert(`Score is\nYou:    ${humanScore}\nComputer: ${computerScore}\n\nComputer Wins!!`)
+        if (humanScore > computerScore) {
+            winner.textContent = `You Win!!`
+            winner.style.color = 'green'
+        } else {
+            winner.textContent = `Computer Wins!!`
+            winner.style.color = 'red'
+
+        }
+
+
+
     }
+    humanScoreElement.textContent = `You:    ${humanScore}`
+    computerScoreElement.textContent = `Computer: ${computerScore}`
+
+
+
+
 }
 
-playGame()
+
+rockBtn.addEventListener("click", () => playButton(1))
+paperBtn.addEventListener("click", () => playButton(2))
+scissorsBtn.addEventListener("click", () => playButton(3))
+
